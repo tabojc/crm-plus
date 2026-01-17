534bb76e476d8080aeecb8e4ec300abd4e5dba11
Juan Taborda <tabordajuan@gmail.com>
2026-01-16 23:09:28 -0400

    refactor: middleware extracted to proxy pattern

--- middleware.ts
+++ middleware.ts
@@ -1,62 +1,8 @@
-
-import { createServerClient } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/lib/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-    let supabaseResponse = NextResponse.next({
-        request,
-    });
-
-    const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return request.cookies.getAll();
-                },
-                setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        request.cookies.set(name, value)
-                    );
-                    supabaseResponse = NextResponse.next({
-                        request,
-                    });
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        supabaseResponse.cookies.set(name, value, options)
-                    );
-                },
-            },
-        }
-    );
-
-    // IMPORTANT: You *must* call getUser to refresh the auth token
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
-
-    // Basic Protection
-    // If no user and asking for dashboard, redirect to login
-    if (!user && request.nextUrl.pathname.startsWith("/dashboard")) {
-        const url = request.nextUrl.clone();
-        url.pathname = "/login";
-        return NextResponse.redirect(url);
-    }
-
-    // If user and asking for login, redirect to dashboard
-    if (user && request.nextUrl.pathname.startsWith("/login")) {
-        const url = request.nextUrl.clone();
-        url.pathname = "/dashboard";
-        return NextResponse.redirect(url);
-    }
-
-    if (user && request.nextUrl.pathname === "/") {
-        const url = request.nextUrl.clone();
-        url.pathname = "/dashboard";
-        return NextResponse.redirect(url);
-    }
-
-    return supabaseResponse;
+    return await updateSession(request)
 }
 
 export const config = {
